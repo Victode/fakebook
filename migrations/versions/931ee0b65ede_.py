2baef44b2f34f38947d64811228f5b8eb8dadf8b
Victor Llagas <rava44311@gmail.com>
2022-09-22 11:02:31 -0500

    first commit

---
+++ migrations/versions/931ee0b65ede_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 931ee0b65ede
+Revises: 6221dbdecb6d
+Create Date: 2022-09-22 10:35:08.985168
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '931ee0b65ede'
+down_revision = '6221dbdecb6d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
